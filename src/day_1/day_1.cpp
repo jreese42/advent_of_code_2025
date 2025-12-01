cc3517a8b1816b646a02346fe4945deab94fdc4c
JD Reese <jreese@alarm.com>
2025-12-01 18:27:19 -0500

    day one

--- src/day_1/day_1.cpp
+++ src/day_1/day_1.cpp
@@ -3,7 +3,6 @@
 #include <string>
 
 std::string part_one(const std::string& input_file) {
-    // TODO: Implement part one solution
     std::ifstream file(input_file);
     if (!file.is_open()) {
         return "Error: Could not open input file";
@@ -11,15 +10,30 @@ std::string part_one(const std::string& input_file) {
     
     // Read and process input
     std::string line;
+    int dial = 50;
+    int acc = 0;
     while (std::getline(file, line)) {
-        // Process each line
+        // we only need an accumulator to solve this, since we only care when landing on 0
+        // first character is positive or negative
+        #define DIAL_MAX 99
+        #define WRAP_AROUND(x) (((x) % (DIAL_MAX+1) + DIAL_MAX + 1) % (DIAL_MAX + 1))
+        if (line[0] == 'L') {
+            // move left
+            dial -= std::stoi(line.substr(1));
+        } else if (line[0] == 'R') {
+            // move right
+            dial += std::stoi(line.substr(1));
+        }
+        dial = WRAP_AROUND(dial);
+        if (dial == 0) {
+            acc++;
+        }
     }
     
-    return "Not implemented yet";
+    return std::to_string(acc);
 }
 
 std::string part_two(const std::string& input_file) {
-    // TODO: Implement part two solution
     std::ifstream file(input_file);
     if (!file.is_open()) {
         return "Error: Could not open input file";
@@ -27,11 +41,30 @@ std::string part_two(const std::string& input_file) {
     
     // Read and process input
     std::string line;
+    int dial = 50;
+    int acc = 0;
     while (std::getline(file, line)) {
-        // Process each line
+        // we only need an accumulator to solve this, since we only care when landing on 0
+        // first character is positive or negative
+        // we need to count any wraparound which means any time dial is OOB after a move
+        #define DIAL_MAX 99
+        #define WRAP_AROUND(x) (((x) % (DIAL_MAX+1) + DIAL_MAX + 1) % (DIAL_MAX + 1))
+        bool was_zero = (dial == 0);
+        if (line[0] == 'L') {
+            // move left
+            dial -= std::stoi(line.substr(1));
+        } else if (line[0] == 'R') {
+            // move right
+            dial += std::stoi(line.substr(1));
+        }
+
+        // count how many times we passed 0, correcting for edge case of starting on 0 and moving left
+        acc += abs(dial / (DIAL_MAX + 1)) + (dial <= 0) - (was_zero && dial < 0 ? 1 : 0);
+        dial = WRAP_AROUND(dial);
+
     }
     
-    return "Not implemented yet";
+    return std::to_string(acc);
 }
 
 int main(int argc, char* argv[]) {
